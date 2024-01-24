13e0d39a54ccb4db269907477d21235c79d75bda
Claudimir Chelepa <cl.chelepa@gmail.com>
2024-01-24 20:59:44 -0300

    commit init

---
+++ src/main/java/br/com/ServiceFinancial/ServiceFinancialApplication.java
@@ -0,0 +1,13 @@
+package br.com.ServiceFinancial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFinancialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceFinancialApplication.class, args);
+	}
+
+}
