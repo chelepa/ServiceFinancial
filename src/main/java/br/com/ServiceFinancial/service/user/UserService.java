81aea832323bd8a72c7b86c6cecf7023a7d1d95f
Claudimir Chelepa <cl.chelepa@gmail.com>
2024-02-06 22:28:37 -0300

    update

--- src/main/java/br/com/ServiceFinancial/service/user/UserService.java
+++ src/main/java/br/com/ServiceFinancial/service/user/UserService.java
@@ -4,6 +4,7 @@ import br.com.ServiceFinancial.dto.users.UserRequestDTO;
 import br.com.ServiceFinancial.dto.users.UserResponseDTO;
 import br.com.ServiceFinancial.entity.UserEntity;
 import br.com.ServiceFinancial.service.base.BaseService;
+import br.com.ServiceFinancial.utils.UtilsBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +17,22 @@ public class UserService extends BaseService {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private UtilsBuilder utilsBuilder;
+
     public UserResponseDTO createUser(UserRequestDTO request) {
         log.info("UserService.createUser - Start - UserRequestDTO: [{}]", request);
         var entity = modelMapper.map(request, UserEntity.class);
         var newEntity = this.saveUser(entity);
-        var response = modelMapper.map(newEntity, UserResponseDTO.class);
+            newEntity.setCategory(utilsBuilder.createListCategoryDefault(newEntity));
+        var entityCategory = this.saveUser(newEntity);
+            entityCategory.getCategory().forEach(item -> item.setListSubCategory(utilsBuilder.createListSubCategoryDefault(item)));
+        var entitySubCategory = this.saveUser(newEntity);
+            entitySubCategory.setYear(utilsBuilder.createListYearsDefault(newEntity));
+        var entityYear = this.saveUser(entitySubCategory);
+            entityYear.getYear().forEach(item -> item.setMonths(utilsBuilder.createListMonths(item)));
+        var entityMonths = this.saveUser(entityYear);
+        var response = modelMapper.map(entityMonths, UserResponseDTO.class);
         log.info("UserService.createUser - End - CategoryResponseDTO: [{}]", response);
         return response;
     }
